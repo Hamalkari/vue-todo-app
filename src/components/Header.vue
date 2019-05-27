6a3fa127a43c4ccc29d09fd2812cc9d3b1a930a1
Halmakari <kosha.1997@bk.ru>
2019-05-27 19:31:49 +0300

    Проинициализировали проект

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  header.header
+    .date-left
+      .date-left__day {{ day }}
+      .date-left__monyear
+
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: new Date()
+    };
+  },
+  computed: {
+    day() {
+      return this.date.getDay();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
